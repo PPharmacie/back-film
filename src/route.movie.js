16303e118c6e2a8329a141a840536e28aa93197a
aguenou <aguenouy96@gmail.com>
2024-05-17 11:53:20 +0200

    Détail d'un film

--- src/route.movie.js
+++ src/route.movie.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const Movie = require('./controller.movie');
 
 router.get("/",Movie.getMovies);
+router.get("/:id",Movie.getMovies);
 
 module.exports = router;
